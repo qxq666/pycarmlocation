8fa1507de9d41e7719e074cedf85735552632c46
qxq666 <1073711956@qq.com>
2019-08-29 16:55:56 +0800

    增加一个例子

--- 13类与对象.py
+++ 13类与对象.py
@@ -421,3 +421,26 @@ class 电动车(出租车):
 
 小李的电动车 = 电动车(2.5, 3, 15)
 小李的电动车.计费()
+
+
+#练习2
+class Person:
+    def __init__(self, name):
+        self.name = name
+        print('大家注意了！')
+    def show(self):
+        print('一个叫“%s”的人来了。' % self.name)
+class Man(Person):
+    def __init__(self):
+        Person.__init__(self, name='范罗苏姆')  # 继承的基础上做一些改变
+        # 上面的括号里也可以写成(self,'范罗苏姆')，但加上参数后清晰一些，代码看起来更清晰。
+    def show(self):
+        Person.show(self)  # 完全继承父类的方法
+    def leave(self):  # 子类定制新方法
+        print('那个叫“%s”的男人留下了他的背影。' % self.name)
+author1 = Person('吉多')
+author1.show()
+author2 = Man()
+author2.show()
+author3 = Man()
+author3.leave()
